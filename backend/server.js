6514b71a3199b80628e105dd9e619ec22cdc7894
Tainá Dreissig <88323837+TainaDr@users.noreply.github.com>
2024-10-22 14:43:38 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const requestIp = require("request-ip"); // Nova biblioteca para capturar IPs
+const requestIp = require("request-ip"); 
 
 // Configurações
 const app = express();
@@ -15,12 +15,13 @@ const port = 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(requestIp.mw()); // Middleware para capturar o IP do usuário
-
+app.use(requestIp.mw()); 
 // Configurações do MongoDB
 const uri =
       "mongodb+srv://tainadreissig14:cc4r4ccVdOAZx4dh@cluster0.z0h59xx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-      // mongodb+srv://tainadreissig:cc4r4ccVdOAZx4dh@cluster0.eji0zrv.mongodb.net/?retryWrites=true&w=majority";
+// mongodb+srv://tainadreissig:cc4r4ccVdOAZx4dh@cluster0.eji0zrv.mongodb.net/?retryWrites=true&w=majority";
+//mongodb+srv://tainadreissig14:<db_password>@cluster0.z0h59xx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -31,27 +32,28 @@ const historySchema = new mongoose.Schema({
       userId: String,
       action: String,
       ipAddress: String,
-      timestamp: { type: Date, default: Date.now }, 
+      timestamp: { type: Date, default: Date.now },
 });
 
 const History = mongoose.model("History", historySchema);
 
 // Verificar a conexão com MongoDB
-app.get("/api/test-connection", async (req, res) => {
+app.get('/api/test-connection', async (req, res) => {
       try {
-            await mongoose.connection.db.admin().ping();
-            res.status(200).send("Conexão com MongoDB estabelecida com sucesso.");
+            // Código para testar a conexão com o MongoDB
+            res.status(200).send('Conexão bem-sucedida');
       } catch (error) {
-            console.error("Erro ao conectar ao MongoDB:", error);
-            res.status(500).send("Erro ao se conectar ao MongoDB");
+            console.error('Erro ao conectar ao MongoDB:', error);
+            res.status(500).send('Erro ao conectar ao MongoDB');
       }
 });
+
 
 // Rota para registrar histórico
 app.post("/api/history", async (req, res) => {
       try {
             const { userId, action } = req.body;
-            const ipAddress = req.clientIp; // Captura o IP do usuário
+            const ipAddress = req.clientIp; 
 
             const newHistory = new History({ userId, action, ipAddress });
             await newHistory.save();
